f68d79ac73682a2281d3c89e3a73bc003657f48d
Ced <cedricrafanan27@gmail.com>
2025-06-08 21:19:58 +0800

    Dynamic retrieval for data from SEC EDGAR

--- SECEdgar.py
+++ SECEdgar.py
@@ -1,12 +1,75 @@
 import requests
+import json
+import time
+import os
+import pandas as pd
+import numpy as np
 
+# New folder to store retreived data from SEC EDGAR
+output_dir = "sec_submissions"
+os.makedirs(output_dir, exist_ok=True)
+
+# Opening the OG dataset with all companies in SEC EDGAR
+with open("company_tickers_exchange.json", "r") as f:
+    CIK_dict = json.load(f)
+
+CIK_df = pd.DataFrame(CIK_dict["data"], columns=CIK_dict["fields"])
+nasdaq_df = CIK_df[CIK_df["exchange"] == "Nasdaq"]
+
+# Ticker list so that can find the nasdaq 100 companies since OG json dataset doesn't specify
+nasdaq_100_tickers = [
+    'AAPL', 'MSFT', 'NVDA', 'AMZN', 'META', 'GOOGL', 'GOOG', 'TSLA', 'ADBE', 'AMD',
+    'ABNB', 'AEP', 'AMGN', 'ADI', 'ANSS', 'APP', 'ARM', 'ASML', 'AZN', 'TEAM',
+    'ADSK', 'ADP', 'AXON', 'BKR', 'BIIB', 'BKNG', 'AVGO', 'CDNS', 'CDW', 'CHTR',
+    'CTAS', 'CSCO', 'CCEP', 'CTSH', 'CMCSA', 'CEG', 'CPRT', 'CSGP', 'COST', 'CRWD',
+    'CSX', 'DDOG', 'DXCM', 'FANG', 'DASH', 'EA', 'EXC', 'FAST', 'FTNT', 'GEHC',
+    'GILD', 'GFS', 'HON', 'IDXX', 'INTC', 'INTU', 'ISRG', 'KDP', 'KLAC', 'KHC',
+    'LRCX', 'LIN', 'LULU', 'MAR', 'MRVL', 'MELI', 'META', 'MCHP', 'MU', 'MSTR',
+    'MDLZ', 'MNST', 'NFLX', 'NXPI', 'ORLY', 'ODFL', 'ON', 'PCAR', 'PLTR', 'PANW',
+    'PAYX', 'PYPL', 'PDD', 'PEP', 'QCOM', 'REGN', 'ROP', 'ROST', 'SHOP', 'SBUX',
+    'SNPS', 'TTWO', 'TMUS', 'TSLA', 'TXN', 'TTD', 'VRSK', 'VRTX', 'WBD', 'WDAY',
+    'XEL', 'ZS'
+]
+
+# Filtering the Nasdaq 100 companies here
+nasdaq_df = nasdaq_df[nasdaq_df["ticker"].isin(nasdaq_100_tickers)].reset_index(drop=True)
+# print(nasdaq_df)
+
+cik_list = []
+
+for ticker in nasdaq_100_tickers:
+    CIKs = nasdaq_df[nasdaq_df["ticker"]==ticker]["cik"].values
+    cik_list.extend(CIKs)
+
+
+# print(cik_list)
+
+# Making the requests and all from SEC EDGAR
 headers = {
     "User-Agent": "rhenzlargo80@gmail.com"
 }
 
-url = "https://data.sec.gov/submissions/CIK0000320193.json"
-response = requests.get(url, headers=headers)
-print(response.json())
+# Testing with only 2 for now since we might overload their servers LOL
+for cik in cik_list[:2]:
+    cik_padded = str(cik).zfill(10)
+    url = f"https://data.sec.gov/submissions/CIK{cik_padded}.json"
+
+    response = requests.get(url,headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        print(f"Data for CIK {cik_padded}")
+
+        file_path = os.path.join(output_dir, f"{cik_padded}.json")
+        with open(file_path, "w") as f:
+             json.dump(data, f, indent=4)
+
+    else:
+        print(f"Failed to fetch data for CIK{cik_padded} (status{response.status_code})")
+
+    time.sleep(0.11)
+
+
 
 
 
